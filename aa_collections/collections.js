5f36f72bfe73bd14b063183b57ded56f026563a8
lucetius <lucetius@lucetius.pl>
2014-03-27 16:47:43 +0100

    add change name for Collections

--- aa_collections/collections.js
+++ aa_collections/collections.js
@@ -14,5 +14,23 @@ Meteor.methods({
         Meteor.users.update({_id: user._id}, {$addToSet: {collections: collection}});
         //Meteor.theBrain.addConnections(10);
         return collection._id;
-    }
+    },
+    
+    updateCollectionName: function (collectionAttributes) {
+        var user = Meteor.user();
+        if (!user)
+            throw new Meteor.Error(401, "You need to login to update collection");
+        if (!collectionAttributes.name) {
+            throw new Meteor.Error(422, "Please fill the name of your collection");
+        }
+                
+        var _collectionId = {_id: user._id, "collections._id": collectionAttributes.collectionId};
+        
+        var _collectionIndex = _.indexOf(_.pluck(Meteor.user().collections, '_id'), collectionAttributes.collectionId);
+
+        var modifier = {$set: {}};
+        modifier.$set["collections." + _collectionIndex + ".name"] = collectionAttributes.name;
+
+        Meteor.users.update(_collectionId, modifier);
+    }  
 })
