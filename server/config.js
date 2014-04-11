419f1ec148a24f247043e2963876ea0e427a5dde
Lukasz Gandecki <lgandecki@css.edu>
2014-04-10 20:19:35 -0500

    working version for 0.7.3

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
-Accounts.loginServiceConfiguration.remove({
+ServiceConfiguration.configurations.remove({
     service: "facebook"
 });
-Accounts.loginServiceConfiguration.insert({
+ServiceConfiguration.configurations.insert({
     service: "facebook",
     appId: "613948438636088",
     secret: "63ca92b902d93a36f7deabdd9bfbc5a8"
